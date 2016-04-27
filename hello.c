7cb321f769c2e19d8868ee9873938114e98453fe
Fransex <francois.cherrier-valentin@epita.fr>
2016-04-27 16:34:56 +0700

    Sending some files

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+	char letter = "A";
+	printf("Hello I'm user %s\n", letter);
+	return 0;
+}
---
+++ numb.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+	int i = 0;
+	for(; i< 12; i++)
+		printf("%d - ",i);
+	printf("%d\n",i);
+	return 0;
+}
