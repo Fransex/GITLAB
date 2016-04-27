ed8f642e50530afed6a1ac84c6057fe5f0c411ac
Dr-LYK <vivian.chaizemartin@epita.fr>
2016-04-27 18:32:51 +0700

    User B - Commit #1

---
+++ alpha.c
@@ -0,0 +1,19 @@
+/* Program #3 */
+
+# include <stdio.h>
+# include <stdlib.h>
+
+int main()
+{
+	char c = A;
+	int i = 0;
+	
+	while (i < 25)
+	{
+		printf("%c -", c);
+		c++;
+		i++;
+	}
+	printf("%c", c);
+	return 0;
+}
--- numb.c
+++ numb.c
@@ -1,11 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
+/* Program #2 */
+
+# include <stdio.h>
+# include <stdlib.h>
 
 int main ()
 {
 	int i = 0;
-	for(; i< 12; i++)
-		printf("%d - ",i);
+	while(i < 12)
+    {
+		if (i % 2 == 0)
+		{
+			printf("%d - ",i);
+		}
+		i++;
+	}
 	printf("%d\n",i);
 	return 0;
 }
